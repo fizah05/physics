4ac453c86cf53797652993a47733ea97f6518ea8
fizah05 <fizah.alislam190506@gmail.com>
2020-03-18 19:07:49 +0000

    completed velocity unit

--- velocity.py
+++ velocity.py
@@ -3,26 +3,43 @@ def velocityCalculator(distance, time):
     speed = distance*time
     return speed
 
-d = 10
-t = 20
+d = int(input("Enter a distance: "))
+t = int(input("Enter a time: "))
 
 s = velocityCalculator(d,t)
 print(s , 'm/s')
 
 
 
-def accelerationCalculator(velocitychange , timetaken):
-
-    velocitychange = endvelocity - initialvelocity
-
-    endvelocity = 30
-    initialvelocity = 10 
-
+def accelerationCalculator( finalvelocity, initialvelocity , timetaken):
+    
+    velocitychange = finalvelocity - initialvelocity
     acceleration = velocitychange/timetaken
     return acceleration
+    
+f = int(input("Enter a final velocity: "))
+i= int(input("Enter an initial velocity: "))
+tt = int(input("Enter a time: "))
 
-v = endvelocity  - initialvelocity
-t = 5
-
-a = accelerationCalculator(v,t)
+a = accelerationCalculator(f,i,tt)
 print(a , "m/s2" )
+
+
+
+def accelerationCalculatordistancebased(finalvelocity,initialvelocity,distance):
+
+    acceleration2 = (finalvelocity*finalvelocity - initialvelocity*initialvelocity)/ 2*distance
+    return acceleration2
+
+f2 = int(input("Enter a final velocity: "))
+i2 = int(input("Enter an initial velocity: "))
+d2 = int(input("Enter a distance: "))
+
+a2 = accelerationCalculatordistancebased(f2,i2,d2)
+print(a2 , "m/s2")
+
+if a2 < 0:
+    print("The object is decelerating ")
+
+else:
+    print("The object is accelerating") 
