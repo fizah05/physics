71489e65b75cef8dce708b235d899c7ec26fc8d3
fizah05 <fizah.alislam190506@gmail.com>
2020-03-20 19:38:46 +0000

    Fifth

--- weight.py
+++ weight.py
@@ -8,3 +8,16 @@ gfs = 10
 
 w = weightCalculator(m , gfs)
 print (w , "N")
+
+
+
+def forceCalculator(mass, acceleration)
+
+    force = mass*acceleration
+    return force
+
+m = int(input("Please enter a mass: "))
+a = int(input("Please enter an acceleration: "))
+
+f = forceCalculator(m , a)
+print (f , "N")
