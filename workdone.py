2b47d4e6a98d191f78a652b330cf7f86eecd42ec
fizah05 <fizah.alislam190506@gmail.com>
2020-04-06 16:20:50 +0100

    updated

--- workdone.py
+++ workdone.py
@@ -8,3 +8,15 @@ d = int(input("Please enter a distance moved: "))
 
 wd = workdoneCalculator(f , d)
 print ("Work Done is: " , wd , "J")
+
+
+def kineticenergyCalculator(mass, speed):
+
+    kineticenergy = 0.5*mass*speed
+    return kineticenergy
+
+m = int(input("Please enter a mass: "))
+s = int(input("Please enter a speed: "))
+
+ke = kineticenergyCalculator(m,s)
+print ("Kinetic Energy is: " , ke , "J")
