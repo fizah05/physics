4c790a44fbbdd6af8a8556f8eef926a777b65b1b
fizah05 <fizah.alislam190506@gmail.com>
2020-04-09 14:58:15 +0100

    fixed

--- gpe.py
+++ gpe.py
@@ -4,8 +4,13 @@ def gpeCalculator(mass,gfs,changeinheight):
     return gpe
 
 m = int(input("Please enter a mass: "))
-ch = int(input("Please enter a height: "))
+ch1 = int(input("Please enter an initial height: "))
+ch2 = int(input("Please enter a final height: "))
 gfs = 10
+
+ch = ch2 - ch1
 
 gpe = gpeCalculator(m, gfs, ch)
 print("Gravitational Potential Energy is: ", gpe, "J")
+
+
